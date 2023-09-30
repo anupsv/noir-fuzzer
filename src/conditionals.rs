2e7ecb012b8db03607aa0ffc9785e24493a630c4
asv <asv@fof.com>
2023-09-29 19:19:29 -0700

    Adding writing to files.

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,67 +1,68 @@
 use std::error::Error;
-use handlebars::Handlebars;
+use handlebars::{Handlebars, RenderError};
 use serde_json::json;
 use crate::random_data_creator::DataType;
-
+use std::fs::File;
+use std::hash::Hash;
+use std::io::prelude::*;
 pub(crate) struct Conditional {
     pub(crate) name: String,
     pub(crate) input_type: DataType
 }
 
 impl Conditional {
-    pub fn print_with_data(&self, input: &Vec<String>) {
+    pub fn print_with_data(&self, input: &Vec<String>, file_name_prefix: &String) {
         match self.name.as_str() {
             "if" => {
-                self.print_if_statements(input).expect("Couldn't print 'if' statements");
+                self.print_if_statements(input, file_name_prefix).expect("Couldn't print 'if' statements");
             }
             "assert" => {
-                self.print_assert_statements(input).expect("Couldn't print 'assert' statements");
+                self.print_assert_statements(input, file_name_prefix).expect("Couldn't print 'assert' statements");
             }
             _ => {}
         }
     }
 
-    pub fn print_if_statements(&self, input: &Vec<String>) -> Result<(), Box<dyn Error>> {
+    pub fn print_if_statements(&self, input: &Vec<String>, file_name_prefix: &String) -> Result<(), Box<dyn Error>> {
         let mut reg = Handlebars::new();
+        let mut file = File::create(format!("/Users/asv/Projects/noir-projects/noir-fuzzer/results/{}-if.rust", file_name_prefix)).expect("creating new file failed.");
 
         for eachInput in input {
-
+            let mut data;
             match self.input_type {
                 DataType::U32 => {
-                    println!(
-                        "{}",
-                        reg.render_template("\nif checkVariable == {{input1}} {println(\"{}\", {{input1}});}", &json!({"input1": eachInput }))?
-                    );
+                    data = reg.render_template("\nif checkVariable == {{input1}} {println!(\"{}\", {{input1}});}", &json!({"input1": eachInput })).expect("Couldn't render if for U32 data.") + "\n";
                 }
                 DataType::String => {
-                    println!(
-                        "{}",
-                        reg.render_template("\nif checkVariable == \"{{input1}}\" {println(\"{}\", {{input1}});}", &json!({"input1": eachInput }))?
-                    );
+                    data = reg.render_template("\nif checkVariable == \"{{input1}}\" {println!(\"{}\", \"{{input1}}\");}", &json!({"input1": eachInput })).expect("couldn't render String data type") + "\n";
                 }
+            }
+
+            match file.write_all(data.as_bytes()) {
+                Ok(_) => {}
+                Err(_) => {println!("couldn't write data for if statements");}
             }
         }
         Ok(())
     }
 
-    pub fn print_assert_statements(&self, input: &Vec<String>) -> Result<(), Box<dyn Error>> {
+    pub fn print_assert_statements(&self, input: &Vec<String>, file_name_prefix: &String) -> Result<(), Box<dyn Error>> {
         let mut reg = Handlebars::new();
+        let mut file = File::create(format!("/Users/asv/Projects/noir-projects/noir-fuzzer/results/{}-assert.rust", file_name_prefix)).expect("creating new file failed.");
 
         for eachInput in input {
-
+            let mut data;
             match self.input_type {
                 DataType::U32 => {
-                    println!(
-                        "{}",
-                        reg.render_template("assert(checkVariable == {{input1}})", &json!({"input1": eachInput }))?
-                    );
+                    data = reg.render_template("assert(checkVariable == {{input1}});", &json!({"input1": eachInput })).expect("Couldn't render assert for u32 data.") + "\n";
                 }
                 DataType::String => {
-                    println!(
-                        "{}",
-                        reg.render_template("assert(checkVariable == \"{{input1}}\")", &json!({"input1": eachInput }))?
-                    );
+                    data = reg.render_template("assert(checkVariable == \"{{input1}}\");", &json!({"input1": eachInput })).expect("Couldn't render assert for String data.") + "\n";
                 }
+            }
+            match file.write_all(data.as_bytes()) {
+                Ok(_) => {}
+                Err(_) => {println!("couldn't write data for assert statements");}
             }
         }
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,34 @@ use std::error::Error;
 mod random_data_creator;
 mod conditionals;
 use strum::IntoEnumIterator;
-
+use std::time::{SystemTime, UNIX_EPOCH};
 
 
 
 
 fn main() {
     let conditionals: [&str; 2] = ["if", "assert"];
+    let file_name_prefix: String = SystemTime::now().duration_since(UNIX_EPOCH).expect("guess we can go to the past huh!").as_secs().to_string();
 
     for eachConditional in conditionals {
         for eachVarType in random_data_creator::DataType::iter() {
 
             match eachConditional {
                 "if" => {
-                    let random_data = random_data_creator::generate_random(eachVarType, 5);
+                    let random_data = random_data_creator::generate_random(eachVarType, 100);
                     let conditional: conditionals::Conditional = conditionals::Conditional {
                         name: String::from("if"),
                         input_type: eachVarType
                     };
-                    conditional.print_with_data(&random_data);
+                    conditional.print_with_data(&random_data, &file_name_prefix);
                 }
                 "assert" => {
-                    let random_data = random_data_creator::generate_random(eachVarType, 5);
+                    let random_data = random_data_creator::generate_random(eachVarType, 100);
                     let conditional: conditionals::Conditional = conditionals::Conditional {
                         name: String::from("assert"),
                         input_type: eachVarType
                     };
-                    conditional.print_with_data(&random_data);
+                    conditional.print_with_data(&random_data, &file_name_prefix);
                 }
                 _ => {}
             }
